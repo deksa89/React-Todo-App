104cd3089eae4470a5740a11edf1e1c1eb480d80
Dean Hudek <hudek.dean@gmail.com>
2023-04-05 12:09:45 +0200

    added job and private tasks

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,36 +3,67 @@ import React, { useState } from "react";
 import './Input.css';
 
 const Input = () => {
-  const [tasks, setTasks] = useState([]);
+  const [jobTasks, setJobTasks] = useState([]);
+  const [privateTasks, setPrivateTasks] = useState([]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    //   console.log("event: ", event)
     const newTask = event.target.elements.task.value;
-    setTasks([...tasks, newTask]);
+    const taskType = event.target.elements.type.value;
+    if (taskType === "job") {
+      setJobTasks([...jobTasks, newTask]);
+    } else {
+      setPrivateTasks([...privateTasks, newTask]);
+    }
     event.target.reset();
   };
 
-  const handleDelete = (index) => {
-    setTasks(tasks.filter((task, i) => i !== index));
+  const handleDelete = (index, taskType) => {
+    if (taskType === "job") {
+      setJobTasks(jobTasks.filter((task, i) => i !== index));
+    } else {
+      setPrivateTasks(privateTasks.filter((task, i) => i !== index));
+    }
   };
 
   return (
     <div className="inp__app">
       <form onSubmit={handleSubmit}>
         <input className="inp__input" type="text" name="task" />
+        <select className="inp__select" name="type">
+          <option value="job">Job Tasks</option>
+          <option value="private">Private Tasks</option>
+        </select>
         <button className="inp__button" type="submit">Add Task</button>
       </form>
-      <ul>
-        {tasks.map((task, index) => (
-          <div className="inp__task" key={index}>
-            <li className="inp__li">
-              {task}
-            </li>
-            <button className="inp__delete" onClick={() => handleDelete(index)}>X</button>
-          </div>
-        ))}
-      </ul>
+      <div className="inp__tasks">
+        <div className="inp__job-tasks">
+          <h2>Job Tasks</h2>
+          <ul>
+            {jobTasks.map((task, index) => (
+              <div className="inp__task" key={index}>
+                <li className="inp__li">
+                  {task}
+                </li>
+                <button className="inp__delete" onClick={() => handleDelete(index, "job")}>X</button>
+              </div>
+            ))}
+          </ul>
+        </div>
+        <div className="inp__private-tasks">
+          <h2>Private Tasks</h2>
+          <ul>
+            {privateTasks.map((task, index) => (
+              <div className="inp__task" key={index}>
+                <li className="inp__li">
+                  {task}
+                </li>
+                <button className="inp__delete" onClick={() => handleDelete(index, "private")}>X</button>
+              </div>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
