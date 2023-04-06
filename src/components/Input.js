8d631e9db1e90124974984efc4cd86ec67505288
Dean Hudek <hudek.dean@gmail.com>
2023-04-06 11:14:24 +0200

    added priority, width adjusted

--- src/components/Input.js
+++ src/components/Input.js
@@ -5,15 +5,14 @@ import './Input.css';
 const Input = () => {
   const [jobTasks, setJobTasks] = useState([]);
   const [privateTasks, setPrivateTasks] = useState([]);
-  // const [currentSymbol, setCurrentSymbol] = useState("X")
-  // const [color, setColor] = useState("#ff0000");
-
-
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newTask = event.target.elements.task.value;
-    const taskType = event.target.elements.type.value;
+    const newTask = event.target.elements.task.value; // npr. newTask:  do dishes
+    const taskType = event.target.elements.type.value; // npr. taskType: Private Tasks
+    
+    // spread operator (...) nam omogucava da kopiramo dio ili cijeli objekt u drugi objekt 
+    // u ovom kodu kreiramo objekt s properties: task, solved i color te ga kopiramo u prazni jobTasks i privateTasks objekt koristeci spread operator  
     if (taskType === "job") {
       setJobTasks([...jobTasks, { task: newTask, solved: false, color: "#ff0000" }]);
     } else {
@@ -29,20 +28,25 @@ const Input = () => {
       setPrivateTasks(privateTasks.filter((task, i) => i !== index));
     }
   };
-
+ 
+  // da bi svaki gumb djelovao neovisno, moramo stvoriti niz objekata koji sadrže stanje svakog task state-a, uključujući njegov status solved i color
+  // koristimo map funkciju da (loop) prodemo kroz jobTasks/privateTasks
+  // za task na zadanom indexu, funkcija updatea solved property (true/false) i color property (crvena/zelena)
+  // updateani taskovi su postavljeni kao novi state na jobTasks i na privateTasks
   const handleSolved = (index, taskType) => {
     if (taskType === "job") {
-      setJobTasks(jobTasks.map((task, i) => (i === index ? { ...task, solved: !task.solved, color: task.solved ? "#ff0000" : "#00ff00" } : task)));
+      setJobTasks(jobTasks.map((task, i) => (i === index ? { ...task, solved: !task.solved, color: task.solved ? "#ff0000" : "#00ff00" } : task)))
     } else {
-      setPrivateTasks(privateTasks.map((task, i) => (i === index ? { ...task, solved: !task.solved, color: task.solved ? "#ff0000" : "#00ff00" } : task)));
+      setPrivateTasks(privateTasks.map((task, i) => (i === index ? { ...task, solved: !task.solved, color: task.solved ? "#ff0000" : "#00ff00" } : task)))
     }
-  };
+  }
+
 
   return (
     <div className="inp__app">
       <form onSubmit={handleSubmit}>
         <input className="inp__input" type="text" name="task" minLength={5} />
-        <select className="inp__select" name="type">
+        <select className="inp__select-type" name="type">
           <option value="job">Job Tasks</option>
           <option value="private">Private Tasks</option>
         </select>
@@ -57,9 +61,12 @@ const Input = () => {
                 <li className="inp__li">
                   {task.task}
                 </li>
-                <button className="inp__solved" onClick={() => handleSolved(index, "job")} style={{ backgroundColor: task.color }}>
-                  {task.solved ? "✓" : "X"}
-                </button>
+                <select className="inp__select-task">
+                  <option>High</option>
+                  <option>Medium</option>
+                  <option>Low</option>
+                </select>
+                <button className="inp__solved" onClick={() => handleSolved(index, "job")} style={{backgroundColor: task.color}}>{task.solved ? "✓" : "X"}</button>
                 <button className="inp__delete" onClick={() => handleDelete(index, "job")}>Delete</button>
               </div>
             ))}
@@ -73,9 +80,12 @@ const Input = () => {
                 <li className="inp__li">
                   {task.task}
                 </li>
-                <button className="inp__solved" onClick={() => handleSolved(index, "private")} style={{ backgroundColor: task.color }}>
-                  {task.solved ? "✓" : "X"}
-                </button>
+                <select className="inp__select-task">
+                  <option>High</option>
+                  <option>Medium</option>
+                  <option>Low</option>
+                </select>
+                <button className="inp__solved" onClick={() => handleSolved(index, "private")} style={{backgroundColor: task.color}}>{task.solved ? "✓" : "X"}</button>
                 <button className="inp__delete" onClick={() => handleDelete(index, "private")}>Delete</button>
               </div>
             ))}
