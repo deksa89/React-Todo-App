1c7b0cab3d40961bc194512387003e90fa6fa9ff
Dean Hudek <hudek.dean@gmail.com>
2023-04-12 14:05:25 +0200

    removed randomizer bg colors, added colors for options

--- src/components/Input.js
+++ src/components/Input.js
@@ -9,9 +9,9 @@ const Input = () => {
   const [jobTasks, setJobTasks] = useState([]);
   const [privateTasks, setPrivateTasks] = useState([]);
 
-const randomLightColors = () => {
-    return "hsl(" + 360 * Math.random() + ',' + (25 + 70 * Math.random()) + '%,' + (85 + 10 * Math.random()) + '%)'
-}
+// const randomLightColors = () => {
+//     return "hsl(" + 360 * Math.random() + ',' + (25 + 70 * Math.random()) + '%,' + (85 + 10 * Math.random()) + '%)'
+// }
 
 const generateRandomId = () => {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -30,9 +30,9 @@ const handleSubmit = (event) => {
   // spread operator (...) nam omogucava da kopiramo dio ili cijeli objekt u drugi objekt 
   // u ovom kodu kreiramo objekt s properties: id, task, solved i color te ga kopiramo u prazni jobTasks i privateTasks objekt koristeci spread operator  
   if (taskType === "job") {
-    setJobTasks([...jobTasks, { id: generateRandomId(), task: newTask, solved: false, color: "#c5b0b0", randomizedColor: randomLightColors()}]);
+    setJobTasks([...jobTasks, { id: generateRandomId(), task: newTask, solved: false, color: "#c5b0b0"}]); // randomizedColor: randomLightColors()
   } else {
-    setPrivateTasks([...privateTasks, { id: generateRandomId(), task: newTask, solved: false, color: "#c5b0b0", randomizedColor: randomLightColors()}]);
+    setPrivateTasks([...privateTasks, { id: generateRandomId(), task: newTask, solved: false, color: "#c5b0b0"}]); // randomizedColor: randomLightColors()
   }
   event.target.elements.task.value = "";
 };
@@ -57,17 +57,32 @@ const handleSolved = (id, taskType) => {
   }
 }
 
+const handleSelectColor = (event) => {
+  const selectedValue = event.target.value; 
+    if (selectedValue === 'high') {event.target.style.color = '#ff0000'; } 
+    else if (selectedValue === 'low') {event.target.style.color = '#3aa500'; } 
+    else if (selectedValue === 'medium') {event.target.style.color = '#ad7a00'; }
+  }
+
 const handleOnDragEnd = (result) => {
+  // prvi dio provjerava da li je draggable item bačen u dropable kontenjer, ako nije nista se ne desava
   if (!result.destination) {
     return;
   }
 
+  // provjerava iz kojeg kontenjera je predmet izvučen 
   const sourceTasks = result.source.droppableId === 'job' ? jobTasks : privateTasks;
+
+  // provjerava u koji je kontenjer izvučeni predmet ubačen
   const destinationTasks = result.destination.droppableId === 'job' ? jobTasks : privateTasks;
 
+  // zatim funkcija briše predmet iz sourceTasks koristeci splice metodu, a obrisani predmet je spremljen u varijablu removed 
   const [removed] = sourceTasks.splice(result.source.index, 1);
+
+  // obrisani predment pomocu splice dodajemo na željenu poziciju => result.destination.index
   destinationTasks.splice(result.destination.index, 0, removed);
 
+  // update-amo jobTasks i privateTasks s promjenama koristeci spread operator
   setJobTasks([...jobTasks]);
   setPrivateTasks([...privateTasks]);
 };
@@ -93,14 +108,14 @@ return (
                 {jobTasks.map((task, index) => (
                   <Draggable key={task.id} draggableId={task.id} index={index}>
                     {(provided) => (
-                      <div className="inp__task" style={{backgroundColor: task.randomizedColor}} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                      <div className="inp__task-job" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                         <li className="inp__li">
                           {task.task}
                         </li>
-                        <select className="inp__select-task" defaultValue="medium">
-                          <option value="high">High</option>
-                          <option value="medium">Medium</option>
-                          <option value="low">Low</option>
+                        <select className="inp__select-task" defaultValue="medium" onChange={handleSelectColor} >
+                          <option className="inp__option-high" value="high">High</option>
+                          <option className="inp__option-medium" value="medium">Medium</option>
+                          <option className="inp__option-low" value="low">Low</option>
                         </select>
                         <button className="inp__solved" onClick={() => handleSolved(task.id, "job")} style={{backgroundColor: task.color}}>{task.solved ? "✓" : "⏳"}</button>
                         <button className="inp__delete" onClick={() => handleDelete(task.id, "job")}>Delete</button>
@@ -121,14 +136,14 @@ return (
                 {privateTasks.map((task, index) => (
                   <Draggable key={task.id} draggableId={task.id} index={index}>
                     {(provided) => (
-                      <div className="inp__task" style={{backgroundColor: task.randomizedColor}} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                      <div className="inp__task-private" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
                         <li className="inp__li">
                           {task.task}
                         </li>
-                        <select className="inp__select-task" defaultValue="medium">
-                          <option value="high">High</option>
-                          <option value="medium">Medium</option>
-                          <option value="low">Low</option>
+                        <select className="inp__select-task" defaultValue="medium" onChange={handleSelectColor}>
+                          <option className="inp__option-high" value="high">High</option>
+                          <option className="inp__option-medium" value="medium">Medium</option>
+                          <option className="inp__option-low" value="low">Low</option>
                         </select>
                         <button className="inp__solved" onClick={() => handleSolved(task.id, "private")} style={{backgroundColor: task.color}}>{task.solved ? "✓" : "⏳"}</button>
                         <button className="inp__delete" onClick={() => handleDelete(task.id, "private")}>Delete</button>
