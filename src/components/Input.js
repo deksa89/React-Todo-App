c526af9ba10a45eefc75e5699bc895fae2228d38
Dean Hudek <hudek.dean@gmail.com>
2023-04-12 11:44:57 +0200

    added drag and drop for generated tasks

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import './Input.css';
+
+
 
 const Input = () => {
   const [jobTasks, setJobTasks] = useState([]);
@@ -25,7 +28,7 @@ const handleSubmit = (event) => {
   const taskType = event.target.elements.type.value; // npr. taskType: Private Tasks
   
   // spread operator (...) nam omogucava da kopiramo dio ili cijeli objekt u drugi objekt 
-  // u ovom kodu kreiramo objekt s properties: task, solved i color te ga kopiramo u prazni jobTasks i privateTasks objekt koristeci spread operator  
+  // u ovom kodu kreiramo objekt s properties: id, task, solved i color te ga kopiramo u prazni jobTasks i privateTasks objekt koristeci spread operator  
   if (taskType === "job") {
     setJobTasks([...jobTasks, { id: generateRandomId(), task: newTask, solved: false, color: "#c5b0b0", randomizedColor: randomLightColors()}]);
   } else {
@@ -34,11 +37,11 @@ const handleSubmit = (event) => {
   event.target.elements.task.value = "";
 };
 
-const handleDelete = (index, taskType) => {
+const handleDelete = (id, taskType) => {
   if (taskType === "job") {
-    setJobTasks(jobTasks.filter((task) => task.id !== index));
+    setJobTasks(jobTasks.filter((task) => task.id !== id));
   } else {
-    setPrivateTasks(privateTasks.filter((task) => task.id !== index));
+    setPrivateTasks(privateTasks.filter((task) => task.id !== id));
   }
 };
  
@@ -46,13 +49,29 @@ const handleDelete = (index, taskType) => {
 // koristimo map funkciju da (loop) prodemo kroz jobTasks/privateTasks
 // za task na zadanom indexu, funkcija updatea solved property (true/false) i color property (crvena/zelena)
 // updateani taskovi su postavljeni kao novi state na jobTasks i na privateTasks
-const handleSolved = (index, taskType) => {
+const handleSolved = (id, taskType) => {
   if (taskType === "job") {
-    setJobTasks(jobTasks.map((task) => task.id === index ? { ...task, solved: !task.solved, color: task.solved ? "#c5b0b0" : "#00ff00" } : task))
+    setJobTasks(jobTasks.map((task) => task.id === id ? { ...task, solved: !task.solved, color: task.solved ? "#c5b0b0" : "#00ff00" } : task))
   } else {
-    setPrivateTasks(privateTasks.map((task) => task.id === index ? { ...task, solved: !task.solved, color: task.solved ? "#c5b0b0" : "#00ff00" } : task))
+    setPrivateTasks(privateTasks.map((task) => task.id === id ? { ...task, solved: !task.solved, color: task.solved ? "#c5b0b0" : "#00ff00" } : task))
   }
 }
+
+const handleOnDragEnd = (result) => {
+  if (!result.destination) {
+    return;
+  }
+
+  const sourceTasks = result.source.droppableId === 'job' ? jobTasks : privateTasks;
+  const destinationTasks = result.destination.droppableId === 'job' ? jobTasks : privateTasks;
+
+  const [removed] = sourceTasks.splice(result.source.index, 1);
+  destinationTasks.splice(result.destination.index, 0, removed);
+
+  setJobTasks([...jobTasks]);
+  setPrivateTasks([...privateTasks]);
+};
+
 
 return (
   <div className="inp__app">
@@ -64,46 +83,66 @@ return (
       </select>
       <button className="inp__button" type="submit">Add Task</button>
     </form>
-    <div className="inp__group-tasks">
-      <div className="inp__job-tasks">
-        <h2>Job Tasks</h2>
-        <ul>
-          {jobTasks.map((task) => (
-              <div className={`inp__task${task.id === 0 ? ' first' : ''}`} style={{backgroundColor: task.randomizedColor}} key={task.id}>
-                <li className="inp__li">
-                  {task.task}
-                </li>
-                <select className="inp__select-task" defaultValue="medium">
-                  <option value="high">High</option>
-                  <option value="medium">Medium</option>
-                  <option value="low">Low</option>
-                </select>
-                <button className="inp__solved" onClick={() => handleSolved(task.id, "job")} style={{backgroundColor: task.color}}>{task.solved ? "✓" : "⏳"}</button>
-                <button className="inp__delete" onClick={() => handleDelete(task.id, "job")}>Delete</button>
-              </div>
-          ))}
-        </ul>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <div className="inp__group-tasks">
+        <div className="inp__job-tasks">
+          <h2>Job Tasks</h2>
+          <Droppable droppableId="job">
+            {(provided) => (
+              <ul {...provided.droppableProps} ref={provided.innerRef}>
+                {jobTasks.map((task, index) => (
+                  <Draggable key={task.id} draggableId={task.id} index={index}>
+                    {(provided) => (
+                      <div className="inp__task" style={{backgroundColor: task.randomizedColor}} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        <li className="inp__li">
+                          {task.task}
+                        </li>
+                        <select className="inp__select-task" defaultValue="medium">
+                          <option value="high">High</option>
+                          <option value="medium">Medium</option>
+                          <option value="low">Low</option>
+                        </select>
+                        <button className="inp__solved" onClick={() => handleSolved(task.id, "job")} style={{backgroundColor: task.color}}>{task.solved ? "✓" : "⏳"}</button>
+                        <button className="inp__delete" onClick={() => handleDelete(task.id, "job")}>Delete</button>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </div>
+        <div className="inp__private-tasks">
+          <h2>Private Tasks</h2>
+          <Droppable droppableId="private">
+            {(provided) => (
+              <ul {...provided.droppableProps} ref={provided.innerRef}>
+                {privateTasks.map((task, index) => (
+                  <Draggable key={task.id} draggableId={task.id} index={index}>
+                    {(provided) => (
+                      <div className="inp__task" style={{backgroundColor: task.randomizedColor}} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                        <li className="inp__li">
+                          {task.task}
+                        </li>
+                        <select className="inp__select-task" defaultValue="medium">
+                          <option value="high">High</option>
+                          <option value="medium">Medium</option>
+                          <option value="low">Low</option>
+                        </select>
+                        <button className="inp__solved" onClick={() => handleSolved(task.id, "private")} style={{backgroundColor: task.color}}>{task.solved ? "✓" : "⏳"}</button>
+                        <button className="inp__delete" onClick={() => handleDelete(task.id, "private")}>Delete</button>
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </div>
       </div>
-      <div className="inp__private-tasks">
-        <h2>Private Tasks</h2>
-        <ul>
-          {privateTasks.map((task) => (
-              <div className="inp__task" style={{backgroundColor: task.randomizedColor}} key={task.id}>
-                <li className="inp__li">
-                  {task.task}
-                </li>
-                <select className="inp__select-task" defaultValue="medium">
-                  <option value="high">High</option>
-                  <option value="medium">Medium</option>
-                  <option value="low">Low</option>
-                </select>
-                <button className="inp__solved" onClick={() => handleSolved(task.id, "private")} style={{backgroundColor: task.color}}>{task.solved ? "✓" : "⏳"}</button>
-                <button className="inp__delete" onClick={() => handleDelete(task.id, "private")}>Delete</button>
-              </div>
-          ))}
-        </ul>
-      </div>
-    </div>
+    </DragDropContext>
   </div>
 );
 };
